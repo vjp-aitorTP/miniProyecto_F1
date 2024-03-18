777051ddd214eba04088b9ee968274b4813fca80
1DAM- Hector Blazquez Gomez <hblazquezg01@iesvjp.es>
2024-03-18 19:58:39 +0100

    añadir metodos de rellenar los monoplaza

--- F1/src/Equipo.java
+++ F1/src/Equipo.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Equipo {
 
     private String nombre;
@@ -10,9 +12,47 @@ public class Equipo {
     }
 
     //Contructor parametrizado de la clase 
-    public Equipo(String nombre, Monoplaza[] monoplazas) {
+    public Equipo(String nombre) {
         this.nombre = nombre;
-        this.monoplazas = monoplazas;
+        this.monoplazas = new Monoplaza[2];
+    }
+
+    //Pedir texto (String)
+    public String pedirString (){
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("    Valor :");
+        String val = scanner.nextLine();
+        return val;
+    }
+
+    //Pedir numero (int)
+    public int pedirNum (){
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("    Valor :");
+        int val = scanner.nextInt();
+        return val;
+    }
+
+    //en este metodo se añaden todos los datos a un objeto de la clase Monoplaza y se devuelve
+    public Monoplaza generarMonoplaza(){
+        System.out.println("Piloto: ");
+        String pilot = pedirString();
+        System.out.println("Velocidad: ");
+        int vel = pedirNum();
+        System.out.println("Num Box: ");
+        int box = pedirNum();
+        System.out.println("Cantidad de vueltas: ");
+        int vueltas = pedirNum();
+
+        Monoplaza monoplaza = new Monoplaza(pilot, vel, box, vueltas);
+        return monoplaza;
+    }
+
+    //Metodo para añador los dos dos monoplazas al arrays
+    public void rellenarArray(){
+        for(int i =0 ; i<monoplazas.length ; i++){
+            monoplazas[i]=generarMonoplaza();
+        }
     }
 
     //Getters y Setters
